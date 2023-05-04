c8c46c20728305e16de03868c27af2be743c94e9
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-05-04 03:51:43 -0300

    fix the installation of vscode

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [[ -n "$repos" ]]; then
 fi
 
 # Installing VsCode
-curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o /tmp/vscode.rpm && \
+curl -L https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64 -o /tmp/vscode.rpm && \
     rpm-ostree install /tmp/vscode.rpm && \
     rm /tmp/vscode.rpm
 
